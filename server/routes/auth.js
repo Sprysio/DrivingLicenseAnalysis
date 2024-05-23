1294021ed4ba04d7989cdcc6f1338f8a5ae0ee13
Sprysio <spryszakadrian@gmail.com>
2024-05-23 20:06:40 +0200

    fixed logging and registering

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -7,13 +7,11 @@ try {
     const { error } = validate(req.body);
     if (error)
         return res.status(400).send({ message: error.details[0].message })
-    const user = await User.findOne({ email: req.body.email })
-
+    const user = await User.findOne({where:{ email: req.body.email }})
+    
     if (!user)
         return res.status(401).send({ message: "Invalid Email or Password" })
-    const validPassword = await bcrypt.compare(
-    req.body.password,
-    user.password
+    const validPassword = await bcrypt.compare(req.body.password, user.password
     )
 
     if (!validPassword)
