827934602f8bb8ddfd615eb1f2980e3bec010ce8
spixxer <filipstrozek@gmail.com>
2024-06-01 12:49:35 +0200

    przycisk wyboru liczby wierszy/ strzałki do zmiany stron

--- client/src/components/Main/index.jsx
+++ client/src/components/Main/index.jsx
@@ -68,8 +68,14 @@ const Main = () => {
         setCurrentPage((prevPage) => (prevPage > 0 ? prevPage - 1 : 0));
     };
 
-    const startIndex = currentPage * rowsPerPage;
-    const endIndex = startIndex + rowsPerPage;
+    const handleRowsPerPageChange = (event) => {
+        const value = event.target.value;
+        setRowsPerPage(value === 'all' ? 'all' : parseInt(value));
+        setCurrentPage(0); // Reset current page to 0
+    };
+
+    const startIndex = currentPage * (rowsPerPage === 'all' ? data.length : rowsPerPage);
+    const endIndex = rowsPerPage === 'all' ? data.length : startIndex + rowsPerPage;
 
     return (
         <div className={styles.main_container}>
@@ -82,6 +88,15 @@ const Main = () => {
                     Logout
                 </button>
             </nav>
+            <div className={styles.table_controls}>
+                <label htmlFor="rowsPerPage">Rows per page:</label>
+                <select id="rowsPerPage" onChange={handleRowsPerPageChange} value={rowsPerPage}>
+                    <option value="20">20</option>
+                    <option value="50">50</option>
+                    <option value="100">100</option>
+                    <option value="all">All</option>
+                </select>
+            </div>
             <div className={styles.table_container}>
                 <table className={styles.table}>
                     <thead>
@@ -110,18 +125,18 @@ const Main = () => {
             </div>
             <div className={styles.pagination}>
                 <button
-                    className={styles.white_btn}
+                    className={styles.arrow_btn}
                     onClick={handleShowPreviousPage}
                     disabled={currentPage === 0}
                 >
-                    Previous 100 rows
+                    &#8592;
                 </button>
                 <button
-                    className={styles.white_btn}
+                    className={styles.arrow_btn}
                     onClick={handleShowNextPage}
                     disabled={endIndex >= data.length}
                 >
-                    Next 100 rows
+                    &#8594;
                 </button>
             </div>
         </div>
